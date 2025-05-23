805630c8452e365d99cf6851521bbd4961e8c986
Bruno Tenório <tenoriobn@gmail.com>
2025-05-23 18:43:16 -0300

    style: :lipstick: 23/05/25 - organiza estilos do newsletter

--- mono-repo/projects/front-user/src/features/Home/sections/Footer/NewsletterForm/index.tsx
+++ mono-repo/projects/front-user/src/features/Home/sections/Footer/NewsletterForm/index.tsx
@@ -1,31 +1,67 @@
 import styled from 'styled-components';
 import EmailIcon from 'public/icons/email.svg';
-import InputBox from 'src/components/InputBox';
 import { StyledButton } from 'src/components/Button';
 
-const StyledInput = styled.input<{ $hasError?: boolean }>`
-  outline: none;
-  background: none;
-  border: none;
-  color: ${({ theme }) => theme.colors['white']};
-  font-size: 1.25rem;
-  width: 100%;
-  
-  &:-webkit-autofill { 
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: white !important;
-  }
+const Styled = {
+  Label: styled.label`
+    display: flex;
+    align-items: center;
+    gap: .5rem;
+    border: 2px solid ${({ theme }) => theme.colors['gray-400']};
+    border-radius: ${({ theme }) => theme.borderRadius['rounded-48']};
+    cursor: text;
+    width: 100%;
+    height: 64px;
+    padding: .5rem .5rem .5rem 2rem;
+    transition: ${({ theme }) => theme.transitions.smoothTransition};
 
-  &::placeholder {
-    color: ${({ theme }) => theme.colors['gray-400']};
-  }
-`;
+    svg {
+      flex-shrink: 0;
+
+      path {
+        stroke: ${({ theme }) => theme.colors['gray-400']};
+        transition: ${({ theme }) => theme.transitions.smoothTransition};
+      }
+    }
+
+    &:has(input:focus), 
+    &:has(input:not(:placeholder-shown)) {
+      border-color: ${({ theme }) => theme.colors['white']};
+
+      svg path {
+        stroke: ${({ theme }) => theme.colors['white']};
+      }
+    }
+  `,
+
+  Input: styled.input<{ $hasError?: boolean }>`
+    outline: none;
+    background: none;
+    border: none;
+    color: ${({ theme }) => theme.colors['white']};
+    font-size: 1.25rem;
+    width: 100%;
+    
+    &:-webkit-autofill { 
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: white !important;
+    }
+
+    &::placeholder {
+      color: ${({ theme }) => theme.colors['gray-400']};
+    }
+  `,
+};
+
+
 
 export default function NewsletterForm() {
   return (
     <form action="">
-      <InputBox $padding='.5rem .5rem .5rem 2rem' icon={<EmailIcon />}>
-        <StyledInput
+      <Styled.Label>
+        <EmailIcon />
+
+        <Styled.Input
           type="email"
           name="email"
           id="email-newsletter"
@@ -49,7 +85,7 @@ export default function NewsletterForm() {
         >
           Assinar
         </StyledButton>
-      </InputBox>
+      </Styled.Label>
     </form>
   );
 }
